f94444a83291b222b03c5ba7e8497c99e94f2208
SirBore <borekirpaul@gmail.com>
2020-01-09 03:25:30 +0300

    Add files via upload

---
+++ instance/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 20:16:49 2017
+
+@author: Thesues
+"""
+
+# instance/config.py
+
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@localhost/dreamteam_db'
